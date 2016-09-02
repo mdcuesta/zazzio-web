b7d94bedf758be921892e27370278d1fd6522863
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2016-09-02 13:18:39 +0800

    webpack set NODE_ENV=production

--- webpack.release.js
+++ webpack.release.js
@@ -47,6 +47,11 @@ module.exports = [{
           warnings: false
         },
         comments: false
+      }),
+      new webpack.DefinePlugin({
+        "process.env": { 
+           NODE_ENV: JSON.stringify("production") 
+         }
       })
     ]
   }, {
