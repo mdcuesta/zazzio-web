fa41d643e2297c1a276843486602e2cd4d13e209
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2016-11-04 19:44:18 +0800

    modified max age for static files

--- path-config.js
+++ path-config.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import path from 'path';
 
+const day = 86400000;
+const week = day * 7;
+
 export default function configure(app) {
-  app.use(express.static(path.join(__dirname, 'assets')));
+  app.use(express.static(path.join(__dirname, 'assets'), { maxAge: week + 1 }));
 }
